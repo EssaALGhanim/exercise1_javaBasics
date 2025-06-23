df0b927d4aa842d6d044b5532da795f6313f5e44
Essa <essaalghinm@gmail.com>
2025-06-23 20:51:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,136 @@
+import java.net.StandardSocketOptions;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+/*
+        // Ex1
+        System.out.println("BMI calculator!" + "\n----------------------------");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your height in meters: ");
+        double meters = input.nextDouble();
+        System.out.print("Enter your weight in kilograms: ");
+        double kg = input.nextDouble();
+
+        double BMI = kg / Math.pow(meters, 2);
+
+        System.out.println("Your BMI is: " + Math.round(BMI * 100) / 100.0);
+*/
+
+
+
+        //Ex2
+
+       /* Scanner input = new Scanner(System.in);
+
+        System.out.print("enter the obtained mark: ");
+        Double obtMark = input.nextDouble();
+        System.out.print("Enter the total mark: ");
+        Double totMark = input.nextDouble();
+
+        double percent = (obtMark * 100) / totMark;
+
+        System.out.println("Percentage= " + percent + "%");*/
+
+      /*  //Ex3
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter amount in USD: ");
+        Double USD = input.nextDouble();
+        System.out.print("Enter Exchange Rate (USD to SAR): ");
+        Double SAR = input.nextDouble();
+
+        System.out.println("Amount in SAR: " + USD * SAR);*/
+
+    /*    //Ex4
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String str = input.nextLine();
+
+        int length = str.length();
+
+        String reversed = new StringBuffer(str).reverse().toString();
+
+        System.out.println("Length of the string: " + length + " And Reversed string: \"" + reversed +"\"");
+        */
+
+      /*  //Ex5
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please a sentence: ");
+        String sentence = input.nextLine();
+
+        System.out.println("Enter start index: ");
+        int startIndex = input.nextInt();
+        System.out.println("Enter end index: ");
+        int endIndex = input.nextInt();
+
+        String sentenceOutput = sentence.substring(startIndex, endIndex);
+        System.out.println("\"" + sentenceOutput + "\"");
+*/
+
+
+      /*  //Ex6
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a sentence: ");
+        String sentence = input.nextLine();
+
+        System.out.println("Enter a keyword to search for: ");
+        String keyword = input.nextLine();
+
+        if (sentence.toLowerCase().contains(keyword.toLowerCase())) {
+            System.out.println("Keyword \"" + keyword + "\" is present in the sentence.");
+        }    else{
+                System.out.println("Keyword \"" + keyword + "\" is not present in the sentence.");
+            }*/
+
+       /* //Ex7
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a sentence: ");
+        String sentence = input.nextLine();
+
+        System.out.println("Enter a word to replace: ");
+        String replace = input.nextLine();
+
+        System.out.println("Enter the replacement word: ");
+        String replacement = input.nextLine();
+
+        String newSentence = sentence.replace(replace, replacement);
+
+        System.out.println("\""+ newSentence+"\"");
+*/
+
+       /* //Ex8
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first string: ");
+        String first = input.nextLine();
+        System.out.println("Enter second string: ");
+        String second = input.nextLine();
+
+        if (first.equalsIgnoreCase(second))
+            System.out.println("Strings are equal (ignore case).");
+        else
+            System.out.println("Strings are not equal (ignore case).");
+*/
+
+
+
+
+
+        }
+
+    }
